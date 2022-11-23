576137411c047dd3d212b59e1a6dec5ece64b95f
Tạ Quang Khôi <khoi0941@gmail.com>
2022-11-23 23:10:49 +0700

    Create a class

---
+++ Two-Sum/Solution.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Admin on 23-Nov-22.
+//
+
+#include "Solution.h"
---
+++ Two-Sum/Solution.h
@@ -0,0 +1,15 @@
+//
+// Created by Admin on 23-Nov-22.
+//
+
+#ifndef TWO_SUM_SOLUTION_H
+#define TWO_SUM_SOLUTION_H
+
+
+class Solution {
+public:
+    
+};
+
+
+#endif //TWO_SUM_SOLUTION_H
